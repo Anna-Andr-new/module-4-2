4b8104c673b04201648803222e2679ccbff03b2d
Anna-Andr-new <tsukanova0910anna@gmail.com>
2024-07-31 17:05:56 +0300

    Add files via upload

---
+++ module 4 2.py
@@ -0,0 +1,20 @@
+def test_function():
+        def inner_function():
+            print("Я в области видимости функции test_function")
+            return print()
+            inner_function()
+
+
+        inner_function()
+test_function()
+
+
+
+
+
+
+
+
+#print (globals())
+# test_function()
+# inner_function()
